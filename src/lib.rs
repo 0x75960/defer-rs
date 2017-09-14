7a726c0b5d10f98c7b57cc8d2beb3d616eeac910
Hiromitsu Oshiba <h.oshiba.0117@gmail.com>
2017-09-15 02:33:07 +0900

    remove: extra namespace

--- src/lib.rs
+++ src/lib.rs
@@ -1,54 +1,51 @@
-pub mod defer {
+/// go-like defer provider
+pub struct Defer<F: FnMut()> {
+    /// proc to do
+    pub defer_proc: Option<F>,
+}
 
-    /// go-like defer provider
-    pub struct Defer<F: FnMut()> {
-        /// proc to do
-        pub defer_proc: Option<F>,
+impl<F: FnMut()> Drop for Defer<F> {
+    /// do defer-proc when drop object
+    fn drop(&mut self) {
+        self.defer_proc.take().unwrap()()
     }
+}
 
-    impl<F: FnMut()> Drop for Defer<F> {
-        /// do defer-proc when drop object
-        fn drop(&mut self) {
-            self.defer_proc.take().unwrap()()
+impl<F: FnMut()> Defer<F> {
+    /// register defer proc
+    pub fn register(p: F) -> Defer<F> {
+        Defer {
+            defer_proc: Some(p),
         }
     }
+}
 
-    impl<F: FnMut()> Defer<F> {
-        /// register defer proc
-        pub fn register(p: F) -> Defer<F> {
-            Defer {
-                defer_proc: Some(p),
-            }
-        }
-    }
-
-    /// # Macro
-    ///
-    /// Usage:
-    ///
-    /// ```
-    /// #[macro_use(defer)]
-    /// extern crate defer;
-    ///
-    /// use defer::Defer;
-    ///
-    /// fn main() {
-    ///     defer!({
-    ///         println!("this will appear 4th!");
-    ///     });
-    ///     defer!({
-    ///         println!("this will appear 2nd!");
-    ///         println!("this will appear 3rd!");
-    ///     });
-    ///     println!("this will appear 1st!");
-    /// }
-    /// ```
-    #[macro_export]
-    macro_rules! defer {
+/// # Macro
+///
+/// Usage:
+///
+/// ```
+/// #[macro_use(defer)]
+/// extern crate defer;
+///
+/// use defer::Defer;
+///
+/// fn main() {
+///     defer!({
+///         println!("this will appear 4th!");
+///     });
+///     defer!({
+///         println!("this will appear 2nd!");
+///         println!("this will appear 3rd!");
+///     });
+///     println!("this will appear 1st!");
+/// }
+/// ```
+#[macro_export]
+macro_rules! defer {
         ($e:expr) => (
             let _scope_call = Defer::register(
                 || -> () { $e; }
             );
         )
-    }
 }
